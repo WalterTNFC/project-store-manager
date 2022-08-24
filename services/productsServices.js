62601f6ff2efa2ded8c1263b5621d6030871656d
WalterTNFC <walter.tad@hotmail.com>
2022-08-23 23:00:57 -0300

    feat: services para produtos- requisitos 10, 12 e 18

--- services/productsServices.js
+++ services/productsServices.js
@@ -13,7 +13,7 @@ async function getAll() {
 async function getById(id) {
   const products = await productsModel.getById(id);
   if (!products) {
-    return { code: 404, message: 'Product not found' };
+    return { error: { code: 404, message: 'Product not found' } };
   }
   return { code: 200, data: products };
 }
@@ -24,8 +24,46 @@ async function newProducts(productName) {
   return { code: 201, id, name };
 }
 
+// Requisito 10
+async function updateProduct(id, name) {
+  const product = await productsModel.getById(id);
+  const update = await productsModel.updateProduct(id, name);
+  
+  if (!product) {
+    return { error: { code: 404, message: 'Product not found' } };
+  }
+  return { code: 200, data: update };
+}
+
+// Requisito 12
+async function deleteProduct(id) {
+  const product = await productsModel.getById(id);
+  if (!product) {
+    return { error: { code: 404, message: 'Product not found' } };
+  }
+
+  await productsModel.deleteProduct(id);
+  return { code: 204 };
+}
+
+// Requisito 18
+async function searchProduct(name) {
+  console.log(name);
+  if (!name) {
+    console.log('sem name');
+    const product = await productsModel.getAll();
+    return { code: 200, data: product };
+  }
+
+  const result = await productsModel.searchProduct(name);
+  console.log(result);
+  return { code: 200, data: result };
+}
 module.exports = {
   getAll,
   getById,
   newProducts,
+  updateProduct,
+  deleteProduct,
+  searchProduct,
 };
