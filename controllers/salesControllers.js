af41464dfb9227ae4d2eb16827f0fb27cb1f7fb2
WalterTNFC <walter.tad@hotmail.com>
2022-08-23 22:57:03 -0300

    feat: Controller para Sales 6, 8, 14 e 16

--- controllers/salesControllers.js
+++ controllers/salesControllers.js
@@ -1,18 +1,66 @@
 const salesService = require('../services/salesServices');
 
-const getAll = async (_req, res) => {
-  const sales = await salesService.getAll();
-
-  res.status(200).json(sales);
+const serverErrorMessage = { code: 500, message: 'Server error.' };
+const getAll = async (_req, res, next) => {
+  try {
+    const { code, data } = await salesService.getAll();
+    return res.status(code).json(data);
+  } catch (error) {
+    next(serverErrorMessage);
+  }
 };
 
-const getByID = async (req, res) => {
-  const sale = await salesService.getByID(req.params);
-
-  res.status(200).json(sale);
+const getById = async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const { error, code, data } = await salesService.getById(id);
+    if (error) return res.status(error.code).json({ message: error.message });
+    return res.status(code).json(data);
+  } catch (error) {
+    next(serverErrorMessage);
+    //
+  }
 };
+
+async function createSale(req, res) {
+  const { code, data } = await salesService.createSale(req.body);
+  return res.status(code).json(data);
+}
+
+// Requisito 14
+async function deleteSale(req, res) {
+  const { id } = req.params;
+  const { error, code } = await salesService.deleteSale(id);
+
+  if (error) {
+    return res.status(error.code).json({ message: error.message });
+  }
+  return res.status(code).end();
+}
+
+// Rquisito 16
+async function updatesale(req, res) {
+  const { id } = req.params;
+
+  const { error, code, data } = await salesService.updateSale(id, req.body);
+
+  if (error) {
+    return res.status(error.code).json({ message: error.message });
+  }
+
+  return res.status(code).json(data);
+}
+
+// async function create(req, res) {
+//   const sale = await salesService.create(req.body);
+
+//   res.status(201).json(sale);
+// }
 
 module.exports = {
   getAll,
-  getByID,
+  getById,
+  createSale,
+  deleteSale,
+  updatesale,
 };
