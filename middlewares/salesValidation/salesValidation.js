45c684a57a69701a175725a4062987a810c456f6
WalterTNFC <walter.tad@hotmail.com>
2022-08-23 23:08:07 -0300

    fix:  validacao para services (test)

--- middlewares/salesValidation/salesValidation.js
+++ middlewares/salesValidation/salesValidation.js
@@ -1,11 +1,11 @@
-const validation = require('../../services/validations/sales');
+// const validation = require('../../services/validations/sales');
 
-const salesValidation = (req, _res, next) => {
-  validation.validateFields(req.body);
+// const salesValidation = (req, _res, next) => {
+//   validation.validateFields(req.body);
 
-  next();
-};
+//   next();
+// };
 
-module.exports = {
-  salesValidation,
-};
+// module.exports = {
+//   salesValidation,
+// };
