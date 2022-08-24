554afb6c47cc650c29332a550cacffe230b92787
WalterTNFC <walter.tad@hotmail.com>
2022-08-23 22:57:51 -0300

    feat: validacao para sales

---
+++ middlewares/salesValidation/salesValidation.js
@@ -0,0 +1,11 @@
+const validation = require('../../services/validations/sales');
+
+const salesValidation = (req, _res, next) => {
+  validation.validateFields(req.body);
+
+  next();
+};
+
+module.exports = {
+  salesValidation,
+};
